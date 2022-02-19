419b8e3afb5a772938bf0fe3e33a27a5ed3799b3
weihyZzz <971973918@qq.com>
2022-02-19 20:57:07 +0800

    2-8 配置了SpringBoot热部署

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.jiawa.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private  String testhello;
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello world!"+testhello;
     }
 
 }
