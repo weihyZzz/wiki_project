982bce33a4b1ba3b9d7f26d0c492e06cc6d40fab
weihyZzz <971973918@qq.com>
2022-02-22 17:50:13 +0800

    22-2-22-17:50 封装请求参数和返回参数

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,17 +1,34 @@
 package com.jiawa.wiki.service;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");//模糊查询
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList=new ArrayList<>();
+        //迭代ebookList中的内容，
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);//将ebook的所有属性赋值到ebookresp
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
