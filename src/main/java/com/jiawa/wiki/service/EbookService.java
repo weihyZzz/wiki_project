5638c69dde6017b07ac8ac6bd2f7e3c4b72d3179
weihyZzz <971973918@qq.com>
2022-02-23 15:50:15 +0800

    22-2-23-15:50 制作工具类CopyUtil，封装BeanUitls

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -6,6 +6,7 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
+import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +23,20 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");//模糊查询
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList=new ArrayList<>();
-        //迭代ebookList中的内容，
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);//将ebook的所有属性赋值到ebookresp
-            respList.add(ebookResp);
-        }
-        return respList;
+//        //将得到的内容转换成EbookResp类型return回去
+//        List<EbookResp> respList=new ArrayList<>();
+//        //迭代ebookList中的内容，
+//        for (Ebook ebook : ebookList) {
+//            EbookResp ebookResp = new EbookResp();
+//            BeanUtils.copyProperties(ebook,ebookResp);//将ebook的所有属性赋值到ebookresp
+//            respList.add(ebookResp);
+//        }
+
+//        return respList; (25-32行方法的返回，)
+
+        //CopyUitl封装的写法，直接复制整个列表
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
+
     }
 }
